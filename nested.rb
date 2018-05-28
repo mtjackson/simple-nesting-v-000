15bdb4ef14da0815279712fb063623292cd20700
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-28 15:44:05 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -78,7 +78,7 @@ def adding_matz
      }
 
 		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-		 programmer_hash[:yukihiro_matsumoto][:languages] = []
+		 programmer_hash[:yukihiro_matsumoto][languages: ["LISP", "C"]] << "LISP", "C"
 
 end
 
